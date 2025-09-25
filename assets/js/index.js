2dd4042b67a7fb49929045e19a2b56b221f666f1
cuong9565 <cuongmanhle0905@gmail.com>
2025-09-25 09:12:08 +0700

    Cuong add SignUp Form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,23 @@
-function ShowLoginForm(){
-    let popUp = document.getElementsByClassName('pop-up')[0].style.display = 'flex';
-    let loginForm = document.getElementsByClassName('login-form-container')[0].style.display = 'block';
-    let signUpForm = document.getElementsByClassName('signup-form-container')[0].style.display = 'none';
+var popUp = document.getElementsByClassName('pop-up')[0]
+var loginForm = document.getElementsByClassName('login-form-container')[0]
+var signUpForm = document.getElementsByClassName('signup-form-container')[0]
 
-    console.log('Login')
+
+function ShowLoginForm(){
+    popUp.style.display = 'flex';
+    loginForm.style.display = 'block';
+    signUpForm.style.display = 'none';
 }
 
 function ShowSignUpForm(){    
-    let popUp = document.getElementsByClassName('pop-up')[0].style.display = 'flex';
-    let loginForm = document.getElementsByClassName('login-form-container')[0].style.display = 'none';
-    let signUpForm = document.getElementsByClassName('signup-form-container')[0].style.display = 'block';
-
-    console.log('Sign Up')
+    popUp.style.display = 'flex';
+    loginForm.style.display = 'none';
+    signUpForm.style.display = 'block';
 }
+
+
+popUp.addEventListener("click", (event)=>{
+    if(!loginForm.contains(event.target) && !signUpForm.contains(event.target)){
+        popUp.style.display = 'none'
+    }
+})
