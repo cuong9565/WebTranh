c9a09e9b555ebecd081cccc0d22e984b9f8f7915
cuong9565 <cuongmanhle0905@gmail.com>
2025-09-25 17:21:44 +0700

    Cuong update icon Login && SignUp Form

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,21 +3,67 @@ var loginForm = document.getElementsByClassName('login-form-container')[0]
 var signUpForm = document.getElementsByClassName('signup-form-container')[0]
 
 
+// Function mở đăng nhập
 function ShowLoginForm(){
     popUp.style.display = 'flex';
     loginForm.style.display = 'block';
     signUpForm.style.display = 'none';
+    document.getElementById('email-login').focus()
 }
 
+// Function mở đăng ký
 function ShowSignUpForm(){    
     popUp.style.display = 'flex';
     loginForm.style.display = 'none';
     signUpForm.style.display = 'block';
+    document.getElementById('user-signup').focus()
+}
+
+// Function đóng popup
+function ClosePopUp(){
+    popUp.style.display = 'none';
+}
+
+// Function hiện mật khẩu
+function ShowPass(id){
+    document.getElementById(id).type = 'text';
+    document.getElementsByClassName('closed-' + id)[0].classList.add('hidden')
+    document.getElementsByClassName('opened-' + id)[0].classList.remove('hidden')
+}
+
+// Function ẩn mật khẩu
+function HiddePass(id){
+    document.getElementById(id).type = 'password';
+    document.getElementsByClassName('closed-' + id)[0].classList.remove('hidden')
+    document.getElementsByClassName('opened-' + id)[0].classList.add('hidden')
 }
 
 
-popUp.addEventListener("click", (event)=>{
+// # Thêm event cho các component # ----------------------------
+// ## Event click ngoài form
+popUp.addEventListener('click', (event)=>{
     if(!loginForm.contains(event.target) && !signUpForm.contains(event.target)){
-        popUp.style.display = 'none'
+        ClosePopUp()
     }
+})
+
+// ## Event hover close img
+document.getElementById('close-black-signup').addEventListener('mouseover', (_)=>{
+    document.getElementById('close-black-signup').classList.add('hidden');
+    document.getElementById('close-white-signup').classList.remove('hidden');
+})
+
+document.getElementById('close-white-signup').addEventListener('mouseout', (_)=>{
+    document.getElementById('close-black-signup').classList.remove('hidden');
+    document.getElementById('close-white-signup').classList.add('hidden');
+})
+
+document.getElementById('close-black-login').addEventListener('mouseover', (_)=>{
+    document.getElementById('close-black-login').classList.add('hidden');
+    document.getElementById('close-white-login').classList.remove('hidden');
+})
+
+document.getElementById('close-white-login').addEventListener('mouseout', (_)=>{
+    document.getElementById('close-black-login').classList.remove('hidden');
+    document.getElementById('close-white-login').classList.add('hidden');
 })
